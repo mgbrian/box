8ebcbd963631b50d2191c64f67a8b43b869a1ea4
Brian <mgbrian@users.noreply.github.com>
2026-03-02 09:23:16 +0000

    Separate out read-only config variables.

--- config.sh
+++ config.sh
@@ -10,24 +10,31 @@ IMAGE_NAME="ubuntu-crd"
 CONTAINER_NAME="remote-desktop"
 # The name of the machine as should appear in Chrome Remote Desktop
 CRD_HOSTNAME="the-box"
-# Load optional local overrides before applying defaults.
+
+# Remote desktop user credentials inside the container.
+# Both of these can be overriden in .env.
+CRD_USER="crduser"
+CRD_PASSWORD="crdpassword"
+
+# Persisted data folders
+# Path to the folder containing persisted CRD config data
+HOST_CONFIG_DIR="$(pwd)/crd-config"
+# The Documents, Downloads and Desktop folders on the VM are synced to this folder
+HOST_HOME_MAP="$(pwd)/vm-home"
+
+
+# ***********************  AVOID EDITING BELOW THIS LINE ***********************
+
+
+# Load optional local overrides.
 if [ -f ./.env ]; then
     set -a
     source ./.env
     set +a
 fi
-# Remote desktop user inside the container
-CRD_USER="${CRD_USER:-crduser}"
-# Remote desktop user's password inside the container
-CRD_PASSWORD="${CRD_PASSWORD:-crdpassword}"
-# Path to the folder containing persisted CRD config data
-HOST_CONFIG_DIR="$(pwd)/crd-config"
-# Path to the folder containing persisted home folder data
-# The Documents, Downloads and Desktop folders on the VM are synced to this folder
-HOST_HOME_MAP="$(pwd)/vm-home"
 
 # Persist (synthetic) machine ID to (optionally) keep connection to CRD between
 # container rebuilds
-HOST_MACHINE_ID="$(pwd)/crd-config/machine-id"
+HOST_MACHINE_ID="$HOST_CONFIG_DIR/machine-id"
 # Essential for this to run on Apple Silicon
 PLATFORM="linux/amd64"
